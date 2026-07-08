3a5bcee306a0b4564fe0e50173ce5e1b6804b470
gh-asc99 <pf.alexsc99@gmail.com>
2026-07-08 21:15:26 +0200

    Correcciones gramaticales hechas

--- src/components/components/DatosPresentacion.jsx
+++ src/components/components/DatosPresentacion.jsx
@@ -5,7 +5,7 @@ const DatosPresentacion = () => {
   return (
     <div className="flex items-center text-white gap-5">
       <h2 className='font-semibold'>
-        <span className='text-2xl'>Portfolio web</span> {" "}
+        <span className='text-2xl'>Porfolio web</span> {" "}
         <span className="font-light">| Alejandro Soler Cruz</span>
       </h2>
       <div className="w-10 h-10 rounded-full">
